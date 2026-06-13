e50fa1a6ef70df496f925df2e2c4acb04f5a7db7
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-06-13 00:51:55 -0300

    feat: implementada a api

---
+++ script.js
@@ -0,0 +1,25 @@
+const cityInput = document.getElementById("city-input");
+const searchBtn = document.getElementById("search-btn");
+const cityInfo = document.querySelector(".city-info");
+
+const apiKey = "bd4b54e2cd361f225ec1cc3db59e923a";
+
+function searchCity() {
+  const cityValue = cityInput.value;
+  console.log(cityValue);
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+  fetch(api)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data.name);
+      cityInfo.innerHTML= `
+      <h2 id="city-name">${data.name}</h2>
+      `
+    });
+}
+
+  searchBtn.addEventListener("click", () => {
+    searchCity();
+  });
+
